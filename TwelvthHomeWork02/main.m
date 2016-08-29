495aa54228f4d6e971084c1e585da7091dc8ce0f
Z <Z@imac.local>
2016-08-29 14:42:52 +0300

    Initial Commit

---
+++ TwelvthHomeWork02/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TwelvthHomeWork02
+//
+//  Created by  Z on 29.08.16.
+//  Copyright © 2016 ItStep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
